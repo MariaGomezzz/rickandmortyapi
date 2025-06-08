8979812c5ae182df764eb9d11424b5442e40f426
Isabel Gómez <mariaisabel201011@gmail.com>
2025-06-07 19:20:51 -0500

    Base para la implementacion de la paginacion

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,5 +1,21 @@
 export default function Pagination() {
+
+  /*
+obtener la pagina actual
+obtener pagina siguiente
+
+Mostrar botonoes de antes siguiente o numero de pagina
+  */
+
+
+
+
+
   return (
-    <div>Pagination</div>
+    <>
+      <div>
+        
+      </div>
+    </>
   )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,10 @@
+// Para cuando se implemente la paginacion
+// export async function getCharacters() {
+//   const response = await fetch(`https://rickandmortyapi.com/api/character?page=${5}`);
+//   const data = await response.json();
+//   return data;
+// }
+
 export async function getCharacters() {
   const response = await fetch("https://rickandmortyapi.com/api/character");
   const data = await response.json();
